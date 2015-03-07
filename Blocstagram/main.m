7432e186dd38ce95a8c115cdc17135e483dde3bf
RH Blanchfield <arttechlife@gmail.com>
2015-03-06 21:14:39 -0800

    Initial Commit

---
+++ Blocstagram/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Blocstagram
+//
+//  Created by RH Blanchfield on 3/6/15.
+//  Copyright (c) 2015 bloc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
